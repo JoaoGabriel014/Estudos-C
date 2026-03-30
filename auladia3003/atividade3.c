45e521f2fbfc5c61f0fa957da30ab80d0b3f9378
JoaoGabriel014 <joaogabriel01401@gmail.com>
2026-03-30 20:43:18 -0300

    começo da atividade de estrutura de dados

---
+++ auladia3003/atividade3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h> 
+struct Funcionario{
+    char nome[50];
+    float salario;
+};
+
+int main(){
+    struct Funcionario funcionarios [4];
+
+    for (int i = 0; i < 4; i++) {
+        printf("Digite o nome do funcionario %d: ", i + 1);
+        scanf("%49s", ))
+    }
+}
